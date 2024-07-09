cf3dfa47409cdfc763b36348e968c613a1d6aad4
Hector Rosales <hndoss@gmail.com>
2024-07-09 12:27:37 -0600

    [AR-63] doc(lesson): Add create lesson README.md instructions
    - Add local database tests
    - Improve Make run command
    - Add docker-compose network
    - Support local endpoint from lambda handler
    - Add debugging lines when adding a new record in database
    - Add create lesson README.md

--- Makefile
+++ Makefile
@@ -9,9 +9,25 @@ clean:
 deploy: clean build
 	sls deploy --verbose
 
-test:
+test: build
 	cd functions/lessons && go test
 	cd internal/database && go test
 
-run:
-	sudo docker-compose up
+run: 
+	sudo docker-compose up -d
+	until curl -s http://localhost:8000 > /dev/null; do echo "Waiting for DynamoDB to start..."; sleep 2; done 
+
+	aws dynamodb create-table \
+  		--table-name lessons \
+  		--attribute-definitions AttributeName=ID,AttributeType=S \
+  		--key-schema AttributeName=ID,KeyType=HASH \
+  		--provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+		--region us-west-1 \
+		--endpoint-url http://localhost:8000
+
+	aws dynamodb list-tables --endpoint-url http://localhost:8000 --region us-west-1
+
+	sudo docker-compose logs -f
+
+stop: 
+	sudo docker-compose down
--- internal/database/localclient.go
+++ internal/database/localclient.go
@@ -3,33 +3,35 @@ package database
 import (
 	"context"
 	"fmt"
-	"testing"
+	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
-func GetLocalClient(t *testing.T) DynamoDBPutItemAPI {
-	t.Helper()
+func GetLocalClient(endpoint string) (*dynamodb.Client, error) {
 
+	log.Println("from GetLocalClient function: ", endpoint)
 	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
 
-		if service == dynamodb.ServiceID && region == "us-west-1" {
+		if service == dynamodb.ServiceID {
 			return aws.Endpoint{
-				PartitionID:   "aws",
-				URL:           "http://localhost:8000",
-				SigningRegion: region,
+				// PartitionID: "aws",
+				URL: endpoint,
+				// SigningRegion: "us-west-1",
 			}, nil
 		}
 		return aws.Endpoint{}, fmt.Errorf("unknown endpoint requested")
 	})
 
-	sdkConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(customResolver))
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithEndpointResolverWithOptions(customResolver),
+	)
 
 	if err != nil {
-		t.Error("can't configure a local endpoint")
+		fmt.Println("can't configure a local endpoint")
 	}
 
-	return dynamodb.NewFromConfig(sdkConfig)
+	return dynamodb.NewFromConfig(sdkConfig), err
 }
